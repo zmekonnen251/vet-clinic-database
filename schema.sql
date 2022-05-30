8e32226f19538669be8cecfdbeb24209b702359f
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-05-30 03:53:08 -0700

    Create an initial database for the vet-clinic project

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
+CREATE Database vet_clinic;
 CREATE TABLE animals (
-    name varchar(100)
+    id  INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg REAL
 );
