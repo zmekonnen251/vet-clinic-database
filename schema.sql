e9df52c8d44237418a8c4cb53282fe87ba9eb5d2
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-06-01 10:07:08 +0100

    Create a owners and species table

--- schema.sql
+++ schema.sql
@@ -1,11 +1,26 @@
 /* Database schema to keep the structure of entire database. */
+DROP Database vet_clinic;
+DROP TABLE animals;
+
 CREATE Database vet_clinic;
+
 CREATE TABLE animals (
-    id  INT AUTO_INCREMENT PRIMARY KEY,
+    id  SERIAL PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg REAL,
-    species VARCHAR(50);
+    species VARCHAR(50)
 );
+
+CREATE TABLE owners{
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INT,
+}
+
+CREATE TABLE species{
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+}
