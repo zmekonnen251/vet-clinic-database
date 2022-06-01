08e369ea3f748a60256a88baac47660375882b08
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-06-01 15:16:28 +0100

    Create queries using join

--- data.sql
+++ data.sql
@@ -1,17 +1,28 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) 
-VALUES ('Agumon','2020-02-03',0,TRUE,10.23),
-      ('Gabumon','2018-11-15',2,TRUE,8.0),
-      ('Pikachu','2021-01-07',1,FALSE,15.00),
-      ('Devimon','2017-05-12',5,TRUE,11.00),
-      ('Charmander','2020-02-08',0,FALSE,11.00),
-      ('Plantmon','2020-11-15',2,TRUE,5.7),
-      ('Squirtle','1993-04-02',3,FALSE,12.13),
-      ('Angemon','2005-04-02',1,TRUE,45.00),
-      ('Boarmon','2005-06-07',7,TRUE,20.04),
-      ('Blossom','1998-10-13',3,TRUE,17.00),
-      ('Ditto','2022-05-14',4,TRUE,22.00);
+INSERT INTO owners (full_name,age)
+VALUES  ('Sam Smith',34),
+        ('Jennifer Orwell',19),
+        ('Bob',45),
+        ('Melody Pond',77),
+        ('Dean Winchester',14),
+        ('Jodie Whittaker',38);
+
+INSERT INTO species(name)
+VALUES ('Pokemon'),('Digimon');
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg,species_id,owner_id) 
+VALUES ('Agumon','2020-02-03',0,TRUE,10.23,2,1),
+      ('Gabumon','2018-11-15',2,TRUE,8.0,2,2),
+      ('Pikachu','2021-01-07',1,FALSE,15.00,1,2),
+      ('Devimon','2017-05-12',5,TRUE,11.00,2,3),
+      ('Charmander','2020-02-08',0,FALSE,11.00,1,4),
+      ('Plantmon','2020-11-15',2,TRUE,5.7,2,3),
+      ('Squirtle','1993-04-02',3,FALSE,12.13,1,4),
+      ('Angemon','2005-04-02',1,TRUE,45.00,2,5),
+      ('Boarmon','2005-06-07',7,TRUE,20.04,2,5),
+      ('Blossom','1998-10-13',3,TRUE,17.00,1,4),
+      ('Ditto','2022-05-14',4,TRUE,22.00,1,null);
 
 
 
--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
 /* Database schema to keep the structure of entire database. */
-DROP Database vet_clinic;
-DROP TABLE animals;
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
 
-CREATE Database vet_clinic;
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INT
+);
+
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
 
 CREATE TABLE animals (
     id  SERIAL PRIMARY KEY,
@@ -11,16 +21,14 @@ CREATE TABLE animals (
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg REAL,
-    species VARCHAR(50)
+    species_id INT,
+    owner_id INT,
+    CONSTRAINT fk_species_id
+        FOREIGN KEY(species_id)
+        REFERENCES species(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_owner_id
+        FOREIGN KEY(owner_id)
+        REFERENCES owners(id)
+        ON DELETE CASCADE
 );
-
-CREATE TABLE owners{
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100),
-    age INT,
-}
-
-CREATE TABLE species{
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-}
